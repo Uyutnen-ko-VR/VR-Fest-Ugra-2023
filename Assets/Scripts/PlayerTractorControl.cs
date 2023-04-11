34e082ed53bb3cf5abc0b6c1f2ccb760daf407a2
PRYANIK48 <freetokdevelop@gmail.com>
2023-04-11 13:17:17 +0300

    2

---
+++ Assets/Scripts/PlayerTractorControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTractorControl : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TractorTrigger"))
+        {
+            print("Smth");
+            gameObject.layer = LayerMask.NameToLayer("Tractor");
+            transform.SetParent(other.transform.parent, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TractorTrigger"))
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            transform.SetParent(other.transform.parent.parent, true);
+        }
+    }
+}
