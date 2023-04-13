f4d26360ea9ccc31eab80a5a476e7c1a78af7004
kolya <dolkovnikolaj@gmail.com>
2023-04-13 11:14:41 +0300

    2

--- Assets/Scripts/forkScript.cs
+++ Assets/Scripts/forkScript.cs
@@ -1,69 +1,99 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class forkScript : MonoBehaviour
 {
-    public Transform forkHandler;
-    public float forkValue = 0, minForkHeight, maxForkHeight;
-    float prevForkValue;
-    public getQuaternionScript leverValue;
-    public enum axis
+    public getQuaternionScript upDownLever, leftRightLever;
+    public Vector2 minMaxForkHeight, minMaxForkWeight;
+
+    public GameObject smallHandler, bigHandler;
+
+    private void Update()
     {
-        X, Y, Z, aX, aY, aZ
-    };
-    public axis leverAxis, forkAxis;
-    void Start()
+        var newHeight = ConvertToDiapason(upDownLever.outputAngle, -90, 90, minMaxForkHeight[0], minMaxForkHeight[1]);
+        var newWeight = ConvertToDiapason(leftRightLever.outputAngle, -90, 90, minMaxForkWeight[0], minMaxForkWeight[1]);
+        
+        print(newHeight + " | " + newWeight);
+
+        smallHandler.transform.localPosition = new Vector3(0, newHeight, 0);
+        bigHandler.transform.localPosition = new Vector3(0, 0, newWeight);
+    }
+    
+    private float ConvertToDiapason(float old, float oldMin, float oldMax, float newMin, float newMax)
     {
-        prevForkValue = forkValue;
+        float oldRange = oldMax - oldMin;
+        float newRange = newMax - newMin;
+        float converted = ((old - oldMin) * newRange / oldRange) + newMin;
+
+        return converted;
     }
 
-    void Update()
-    {
-        
-        switch (leverAxis)
-        {
-            case axis.X:
-                forkValue += leverValue.output.x;
-                break;
-            case axis.Y:
-                forkValue += leverValue.output.y;
-                break;
-            case axis.Z:
-                forkValue += leverValue.output.z;
-                break;
-            case axis.aX:
-                forkValue -= leverValue.output.x;
-                break;
-            case axis.aY:
-                forkValue -= leverValue.output.y;
-                break;
-            case axis.aZ:
-                forkValue -= leverValue.output.z;
-                break;
-        }
-        forkValue = Mathf.Clamp(forkValue, minForkHeight, maxForkHeight);
-        switch (forkAxis)
-        {
-            case axis.X:
-                forkHandler.Translate((forkValue - prevForkValue) * Time.deltaTime, 0, 0);
-                break;
-            case axis.Y:
-                forkHandler.Translate(0, (forkValue - prevForkValue) * Time.deltaTime, 0);
-                break;
-            case axis.Z:
-                forkHandler.Translate(0, 0, (forkValue - prevForkValue) * Time.deltaTime);
-                break;
-            case axis.aX:
-                forkHandler.Translate((forkValue - prevForkValue) * -Time.deltaTime, 0, 0);
-                break;
-            case axis.aY:
-                forkHandler.Translate(0, (forkValue - prevForkValue) * -Time.deltaTime, 0);
-                break;
-            case axis.aZ:
-                forkHandler.Translate(0, 0, (forkValue - prevForkValue) * -Time.deltaTime);
-                break;
-        }
-        prevForkValue = forkValue;
-    }
 }
+
+
+// {
+//     public Transform forkHandler;
+//     public float forkValue = 0, minForkHeight, maxForkHeight;
+//     float prevForkValue;
+//     public getQuaternionScript leverValue;
+//     public enum axis
+//     {
+//         X, Y, Z, aX, aY, aZ
+//     };
+//     public axis leverAxis, forkAxis;
+//     void Start()
+//     {
+//         prevForkValue = forkValue;
+//     }
+//
+//     void Update()
+//     {
+//         
+//         switch (leverAxis)
+//         {
+//             case axis.X:
+//                 forkValue += leverValue.output.x;
+//                 break;
+//             case axis.Y:
+//                 forkValue += leverValue.output.y;
+//                 break;
+//             case axis.Z:
+//                 forkValue += leverValue.output.z;
+//                 break;
+//             case axis.aX:
+//                 forkValue -= leverValue.output.x;
+//                 break;
+//             case axis.aY:
+//                 forkValue -= leverValue.output.y;
+//                 break;
+//             case axis.aZ:
+//                 forkValue -= leverValue.output.z;
+//                 break;
+//         }
+//         forkValue = Mathf.Clamp(forkValue, minForkHeight, maxForkHeight);
+//         switch (forkAxis)
+//         {
+//             case axis.X:
+//                 forkHandler.Translate((forkValue - prevForkValue) * Time.deltaTime, 0, 0);
+//                 break;
+//             case axis.Y:
+//                 forkHandler.Translate(0, (forkValue - prevForkValue) * Time.deltaTime, 0);
+//                 break;
+//             case axis.Z:
+//                 forkHandler.Translate(0, 0, (forkValue - prevForkValue) * Time.deltaTime);
+//                 break;
+//             case axis.aX:
+//                 forkHandler.Translate((forkValue - prevForkValue) * -Time.deltaTime, 0, 0);
+//                 break;
+//             case axis.aY:
+//                 forkHandler.Translate(0, (forkValue - prevForkValue) * -Time.deltaTime, 0);
+//                 break;
+//             case axis.aZ:
+//                 forkHandler.Translate(0, 0, (forkValue - prevForkValue) * -Time.deltaTime);
+//                 break;
+//         }
+//         prevForkValue = forkValue;
+//     }
+// }
--- Assets/Scripts/getQuaternionScript.cs
+++ Assets/Scripts/getQuaternionScript.cs
@@ -2,29 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class getQuaternionScript : MonoBehaviour
 {
-    public Quaternion output;
+    // public Quaternion output;
     public float outputAngle;
-    public Transform doll;
+    // public Transform doll;
 
     private HingeJoint _joint;
+    private XRGrabInteractable _grab;
 
     private void Start()
     {
         _joint = GetComponent<HingeJoint>();
+        _grab = GetComponent<XRGrabInteractable>();
     }
 
     void Update()
     {
-        outputAngle = _joint.angle;
-        output = doll.localRotation;
-        if (output.x < 0.001f)
-            output.x = 0;
-        if (output.y < 0.001f)
-            output.y = 0;
-        if (output.z < 0.001f)
-            output.z = 0;
+        if (_grab.isSelected)
+        {
+            print("Hovered");
+            outputAngle = _joint.angle;
+        }
+        else if (outputAngle != 0)
+        {
+            outputAngle = 0;
+        }
+        // output = doll.localRotation;
+        // if (output.x < 0.001f)
+        //     output.x = 0;
+        // if (output.y < 0.001f)
+        //     output.y = 0;
+        // if (output.z < 0.001f)
+        //     output.z = 0;
     }
 }
